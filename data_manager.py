c91316a3bb3d30ac94a9c3266bcc4d6c90f1e8a4
krisszcode <nagy.ace.krisztian@gmail.com>
2019-11-11 14:53:30 +0100

    alma

---
+++ data_manager.py
@@ -0,0 +1,19 @@
+def imports_from_file(filename):
+    with open(filename, "r") as file_handle:
+        f = file_handle.readlines()
+        lista = []
+        for lines in f:
+            lines = lines[:-1]
+            lines = lines.split(",")
+            lista.append(lines)
+        return lista
+
+
+def export_to_file(filename,table):
+    with open(filename, "w") as file_handle:
+        for record in table:
+            row = ';'.join(record)
+            row.replace(";", ",")
+            file_handle.write(row + "\n")
+
+

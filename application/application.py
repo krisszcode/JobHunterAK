cd18189de320264228af498d4ee5bf67dbd2ceb4
krisszcode <nagy.ace.krisztian@gmail.com>
2019-11-14 09:59:13 +0100

    create

--- application/application.py
+++ application/application.py
@@ -29,20 +29,46 @@ def choose():
     table = data_manager.imports_from_file(myfile)
     if option == "1":
         ui.clear()
-        data_manager.export_to_file(myfile, create_applicaion(table))
-    elif option == "2":
-        ui.clear()
-        idx = ui.get_inputs("Enter the student ID: ")
-        if common.check_valid_id(table, idx) == False:
+        companies = data_manager.imports_from_file("company/companies.txt")
+        comp_name = ui.get_inputs("Enter a company name to apply: ")
+        positions = data_manager.imports_from_file("position/positions.txt")
+        student_id = ui.get_inputs("Enter a student id to apply: ")
+        students = data_manager.imports_from_file("student/students.txt")
+        opt = ui.get_inputs("Enter the status(applied/not applied): ")
+        if common.check_valid_id(companies, comp_name) == False or common.check_valid_id(students, student_id) == False:
             ui.clear()
-            ui.print_error(f"Invalid student ID! ('{idx}')")
+            ui.print_error("Non existing company or student!")
         else:
-            ui.print_result(read_student(table, idx), "Student details")
+            if opt != "applied" and opt != "not applied":
+                ui.clear()
+                ui.print_error("Thats not an option")
+            else:    
+                data_manager.export_to_file(myfile, create_applicaion(table, opt, comp_name, student_id, companies))
+    elif option == "2":
+        pass
     elif option == "3":
         pass
+    elif option == "0":
+        ui.clear()
+        return False
+    else:
+        raise KeyError(f"There is no such option. ({option})")
+    return True
 
-def create_company(table):
-    
-    options = ["Enter the application status: "]
-    table.append(common.create_element(table, options))
+def create_applicaion(table, opt, comp_name, student_id, companies):
+    comp_id = -1
+    for row in companies:
+        if row[1] == comp_name:
+            comp_id = row[0]
+    ops = common.create_element(table, [opt])
+    ops.pop()
+    ops.append(opt)
+    temp = []
+    for i in ops:
+        temp.append(i)
+    temp.append(student_id)
+    temp.append(comp_id)
+    kakao = []
+    kakao.extend(temp)
+    table.append(kakao)
     return table
